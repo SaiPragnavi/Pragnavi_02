6d1848fb1141ebc0bf645577a4ccc04d53f45f8c
SaiPragnavi <pragnaviperumalla@gmail.com>
2021-06-18 10:39:34 +0530

    Meetup Search API

---
+++ MeetupSearch/MeetupSearch/Controllers/MeetupController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MeetupSearch.Model;
+using MeetupSearch;
+namespace MeetupSearch.Controllers
+{
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class MeetupController : ControllerBase
+    {
+        string[] registered = { "azure", "aws", "ML" };
+        string search = "ML";
+        public List<string> searchByName(string search)
+        {
+
+            List<string> l = new List<string>();
+            if (!String.IsNullOrEmpty(search))
+            {
+                for(int i = 0; i < registered.Length; i++)
+                {
+                    if (registered[i].Equals(search))
+                    {
+                        l.Add(search);
+                        return l;
+                    }
+                }
+            }
+            l.Add(search + " Not found");
+            return l;
+        }
+
+    }
+}
---
+++ MeetupSearch/MeetupSearch/Model/Meetup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetupSearch.Model
+{
+    public class Meetup
+    {
+        public int mID { get; set; }
+        public string mName { get; set; }
+
+        public string mLocation { get; set; }
+
+        //int[] id = { 1, 2, 3 };
+        //string[] name =
+        //{
+        //    "azure","aws","ML"
+        //};
+        //string[] location =
+        //{
+        //    "hyd","blr","hyd"
+        //};
+    }
+}
